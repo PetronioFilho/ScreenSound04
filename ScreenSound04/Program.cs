01ac74500250874406632b28acd5026f9d90177d
PetronioFilho <camposfilho84@gmail.com>
2025-11-26 16:28:07 -0300

    Initial project commit

---
+++ ScreenSound04/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+//O Http Client é usado para fazer requisições HTTP em C# e permite enviar e receber dados de servidores web.
+using (HttpClient client = new HttpClient())
+{
+    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    Console.WriteLine(resposta);
+}
+
+Console.ReadKey();
